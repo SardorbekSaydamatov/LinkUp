dad6033ea9507af9d5092e8750f5561c9fa7104f
SardorbekSaydamatov <saidalo.saydamatov@gmail.com>
2024-11-11 11:48:43 +0500

    Initial Commit

---
+++ LinkUp/LinkUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkUpApp.swift
+//  LinkUp
+//
+//  Created by Sardorbek Saydamatov on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LinkUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LinkUpUITests/LinkUpUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  LinkUpUITestsLaunchTests.swift
+//  LinkUpUITests
+//
+//  Created by Sardorbek Saydamatov on 11/11/24.
+//
+
+import XCTest
+
+final class LinkUpUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
